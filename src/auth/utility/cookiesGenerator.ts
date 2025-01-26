d6c7f897670022285bff55edcb27a4dd48c464fe
OttoMarcus <invictum@zoho.com>
2025-01-26 23:12:06 +0200

    feat(cookies): create cookies response object

---
+++ src/auth/utility/cookiesGenerator.ts
@@ -0,0 +1,13 @@
+import { Response } from 'express';
+
+export const cookiesGenerator = (res: Response, refreshToken: string) => {
+  res.cookie('refreshToken', refreshToken, {
+    httpOnly: true,
+    secure: true,
+    sameSite: 'lax',
+    maxAge: parseInt(process.env.JWT_REFRESH_EXPIRATION) * 24 * 60 * 60 * 1000,
+  });
+};
+
+
+// Simple func for accept refresh token and set cookie
