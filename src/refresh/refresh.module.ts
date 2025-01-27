1b21db5a6b01324c8d44db60181e513789ec0dc5
OttoMarcus <invictum@zoho.com>
2025-01-27 14:37:41 +0200

    refactor(Auth service): configure env global

--- src/refresh/refresh.module.ts
+++ src/refresh/refresh.module.ts
@@ -3,9 +3,13 @@ import { RefreshService } from './refresh.service';
 import { SequelizeModule } from '@nestjs/sequelize';
 import { RefreshToken } from './models/refresh.model';
 import { JwtModule } from '@nestjs/jwt';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
     SequelizeModule.forFeature([RefreshToken]),
     JwtModule.register({
       global: true,
