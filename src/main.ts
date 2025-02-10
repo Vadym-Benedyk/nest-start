bee3abe21bfc5219043d9631c61735ff3b1b65bf
Vadym-Benedyk <vadym.benedyk@fivesysdev.com>
2025-02-10 12:12:59 +0100

    refactor: commit before typeORM

--- src/main.ts
+++ src/main.ts
@@ -3,11 +3,13 @@ import { AppModule } from './app.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { ValidationPipe } from '@nestjs/common';
 import * as dotenv from 'dotenv';
+import * as process from 'node:process';
 dotenv.config();
 
 async function bootstrap() {
   // await checkAndCreateDatabase();
   // await syncTables()
+
   const app = await NestFactory.create(AppModule, {
     abortOnError: false,
     logger: ['log', 'error', 'warn', 'debug', 'verbose']
