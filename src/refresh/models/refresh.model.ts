1761c53aaf1ad748cc56a73d89c522d921086aa1
Vadym-Benedyk <vadym.benedyk@fivesysdev.com>
2025-02-11 20:46:14 +0100

    feat(Chapter & Topic) entity with typeORM

--- src/refresh/models/refresh.model.ts
+++ src/refresh/models/refresh.model.ts
@@ -1,6 +1,7 @@
 import { Table, Model, Column, DataType } from 'sequelize-typescript';
 import { RefreshInterfaces } from '@/src/database/postgres/sequelize/interfaces/refresh.interfaces';
 
+
 @Table({
   tableName: 'refresh_tokens',
   timestamps: true,
