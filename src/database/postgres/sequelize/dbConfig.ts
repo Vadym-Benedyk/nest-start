617471226b3a72b7b7411d3200b5b3edcb6854e8
Vadym-Benedyk <vadym.benedyk@fivesysdev.com>
2025-05-03 22:35:51 +0300

    feat(Database): add dialect to base configuration

--- src/database/postgres/sequelize/dbConfig.ts
+++ src/database/postgres/sequelize/dbConfig.ts
@@ -1,11 +1,13 @@
 import {
   baseConfigInterface
 } from '@/src/database/postgres/sequelize/interfaces/dbConfig.interface';
+import * as process from 'node:process';
 
 
 export const baseConfig: baseConfigInterface = {
   models: [__dirname + '/entities/*.models.js'],
   url: process.env.DATABASE_URL,
+  dialect: process.env.DATABASE_DIALECT,
   synchronize: false,
   autoLoadModels: true,
 };
--- src/database/postgres/sequelize/interfaces/dbConfig.interface.ts
+++ src/database/postgres/sequelize/interfaces/dbConfig.interface.ts
@@ -3,6 +3,7 @@ import 'dotenv/config';
 export interface baseConfigInterface {
   models: string[];
   url: string;
+  dialect: string;
   synchronize: boolean;
   autoLoadModels: boolean;
 }
