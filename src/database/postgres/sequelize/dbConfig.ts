05eb87ff15ae155562da6c4ce14644f5e8c56f4d
Vadym-Benedyk <vadym.benedyk@fivesysdev.com>
2025-05-03 22:54:51 +0300

    feat(Database): update dialect type to use Sequelize Dialect

--- src/database/postgres/sequelize/dbConfig.ts
+++ src/database/postgres/sequelize/dbConfig.ts
@@ -8,7 +8,8 @@ import { Dialect } from 'sequelize';
 export const baseConfig: baseConfigInterface = {
   models: [__dirname + '/entities/*.models.js'],
   url: process.env.DATABASE_URL,
-  dialect: process.env.DATABASE_DIALECT as Dialect,
+  // dialect: process.env.DATABASE_DIALECT as Dialect,
+  dialect: 'postgres' as Dialect,
   synchronize: false,
   autoLoadModels: true,
 };
